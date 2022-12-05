033df710bd844b8aaf1135929ee644303bbcae46
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-05 23:18:28 +0300

    create tables

--- queries.sql
+++ queries.sql
@@ -2,10 +2,10 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name FROM animals WHERE neutered=true AND escape_attempts<3;
-SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
-SELECT * FROM animals WHERE neutered=true;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
@@ -44,3 +44,4 @@ SELECT species.name, COUNT(*) FROM animals JOIN species ON animals.species_id = 
 SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.id = 2 AND animals.species_id = 2;
 SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.id = 5 AND animals.escape_attempts = 0;
 SELECT owners.full_name, COUNT(*) AS total FROM animals JOIN owners ON animals.owner_id = owners.id GROUP BY owners.full_name ORDER BY total DESC LIMIT 1;
+
