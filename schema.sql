447baaa97ace186a17e683da358a4d10ad6543b2
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-05 21:30:43 +0300

    update and add data

--- schema.sql
+++ schema.sql
@@ -23,3 +23,10 @@ CREATE TABLE species (
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
+
+ALTER TABLE animals ADD PRIMARY KEY(id);
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
