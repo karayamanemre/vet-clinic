11f214c5a7c9f91a2a235b5903ff4ca4dc0dc8dc
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-05 18:12:36 +0300

    updated values

--- queries.sql
+++ queries.sql
@@ -35,4 +35,4 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
